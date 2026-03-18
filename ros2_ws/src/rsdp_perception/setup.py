90c40decac3420ba0f92ef2e30cb9e620dce9290
Alex Inch <alexinch96@yahoo.com>
2026-03-18 17:25:39 +0000

    merge with existing interface package

---
+++ ros2_ws/src/rsdp_perception/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'rsdp_perception'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='laptop30',
+    maintainer_email='jinweizhang2000@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'yolov5_realsense_node = rsdp_perception.yolov5_realsense_node:main',
+        ],
+    },
+)
