3ce82a4bcd4a83229c84ed2c0a09ec132b637ad9
Mike Fahey <michaelfahey42@gmail.com>
2026-05-01 21:45:25 +0100

    combined shell script for setup

--- ros2_ws/NUC_FASTDDS.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-fastdds discovery --server-id=0 --udp-port 11811
--- ros2_ws/NUC_ROS_SETUP.sh
+++ ros2_ws/NUC_ROS_SETUP.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
+
+FASTDDS_SERVER_ID=0
+FASTDDS_UDP_PORT=11811
+FASTDDS_PATTERN="fastdds discovery --server-id=${FASTDDS_SERVER_ID} --udp-port ${FASTDDS_UDP_PORT}"
+FASTDDS_CMD=(fastdds discovery --server-id="${FASTDDS_SERVER_ID}" --udp-port "${FASTDDS_UDP_PORT}")
+
 export ROS_DISCOVERY_SERVER="10.0.0.20:11811"
+
+if pgrep -f "${FASTDDS_PATTERN}" >/dev/null; then
+    echo "Fast DDS discovery server is already running."
+else
+    echo "Starting Fast DDS discovery server on UDP port ${FASTDDS_UDP_PORT}."
+    "${FASTDDS_CMD[@]}"
+fi
+
 . ./install/setup.bash
